32c3b23694a8c38226d9960206315abb6b11a9c8
Ihor Pavliuk <akira.pavl@gmail.com>
2025-04-06 22:12:26 +0300

    restructured the code a bit

--- app/main.ts
+++ app/main.ts
@@ -1,100 +1,112 @@
 import fs from "node:fs";
 
-const args = process.argv.slice(2);
+// todo add function for checking next character
+// todo put all scans into a variable and output it
+// todo create a type/class for tokens
 
-if (args.length < 2) {
-  console.error("Usage: ./your_program.sh tokenize <filename>");
-  process.exit(1);
-}
+function tokenize(filename: string) {
+  let fileContent;
 
-const command = args[0];
-
-if (command !== "tokenize") {
-  console.error(`Usage: Unknown command: ${command}`);
-  process.exit(1);
-}
-
-const filename = args[1];
-
-if (!filename) {
-  console.error(`No file specified for tokenizer`);
-  process.exit(1);
-}
-
-let fileContent;
-
-try {
-  fileContent = fs.readFileSync(filename, "utf8");
-}
-catch (err) {
-  if (err instanceof Error) {
-    console.error(err.message);
+  try {
+    fileContent = fs.readFileSync(filename, "utf8");
   }
-  else {
-    console.error(err);
-  }
-  process.exit(1);
-}
-
-let hasCompileErrors = false;
-
-const fileLines = fileContent.split('\n');
-for (let fileLine of fileLines) {
-  for (let j = 0; j < fileLine.length; ++j)
-    switch (fileLine[j]) {
-      case '(': {
-        console.log('LEFT_PAREN ( null');
-        break;
-      }
-      case ')': {
-        console.log('RIGHT_PAREN ) null');
-        break;
-      }
-      case '{': {
-        console.log('LEFT_BRACE { null');
-        break;
-      }
-      case '}': {
-        console.log('RIGHT_BRACE } null');
-        break;
-      }
-      case ',': {
-        console.log('COMMA , null');
-        break;
-      }
-      case '.': {
-        console.log('DOT . null');
-        break;
-      }
-      case '-': {
-        console.log('MINUS - null');
-        break;
-      }
-      case '+': {
-        console.log('PLUS + null');
-        break;
-      }
-      case ';': {
-        console.log('SEMICOLON ; null');
-        break;
-      }
-      case '/': {
-        console.log('SLASH / null');
-        break;
-      }
-      case '*': {
-        console.log('STAR * null');
-        break;
-      }
-      default: {
-        hasCompileErrors = true;
-        console.error(`[line 1] Error: Unexpected character: ${fileLine[j]}`);
-      }
+  catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
     }
+    else {
+      console.error(err);
+    }
+    process.exit(1);
+  }
+
+  let hasCompileErrors = false;
+
+  const fileLines = fileContent.split('\n');
+  for (let fileLine of fileLines) {
+    for (let j = 0; j < fileLine.length; ++j)
+      switch (fileLine[j]) {
+        case '(': {
+          console.log('LEFT_PAREN ( null');
+          break;
+        }
+        case ')': {
+          console.log('RIGHT_PAREN ) null');
+          break;
+        }
+        case '{': {
+          console.log('LEFT_BRACE { null');
+          break;
+        }
+        case '}': {
+          console.log('RIGHT_BRACE } null');
+          break;
+        }
+        case ',': {
+          console.log('COMMA , null');
+          break;
+        }
+        case '.': {
+          console.log('DOT . null');
+          break;
+        }
+        case '-': {
+          console.log('MINUS - null');
+          break;
+        }
+        case '+': {
+          console.log('PLUS + null');
+          break;
+        }
+        case ';': {
+          console.log('SEMICOLON ; null');
+          break;
+        }
+        case '/': {
+          console.log('SLASH / null');
+          break;
+        }
+        case '*': {
+          console.log('STAR * null');
+          break;
+        }
+        default: {
+          hasCompileErrors = true;
+          console.error(`[line 1] Error: Unexpected character: ${fileLine[j]}`);
+        }
+      }
+  }
+
+  console.log("EOF  null");
+
+  if (hasCompileErrors) {
+    process.exit(65);
+  }
 }
 
-console.log("EOF  null");
+function main() {
+  const args = process.argv.slice(2);
 
-if (hasCompileErrors) {
-  process.exit(65);
+  if (args.length < 2) {
+    console.error("Usage: ./your_program.sh tokenize <filename>");
+    process.exit(1);
+  }
+
+  const command = args[0];
+
+  if (command !== "tokenize") {
+    console.error(`Usage: Unknown command: ${command}`);
+    process.exit(1);
+  }
+
+  const filename = args[1];
+
+  if (!filename) {
+    console.error(`No file specified for tokenizer`);
+    process.exit(1);
+  }
+
+  tokenize(filename);
 }
+
+main();
