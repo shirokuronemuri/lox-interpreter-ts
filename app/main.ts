10c655ca334f2777d5dc8fbdfb50848a657b2e41
Ihor Pavliuk <akira.pavl@gmail.com>
2025-04-07 18:02:32 +0300

    tokenizer rewritten from function to class

--- app/main.ts
+++ app/main.ts
@@ -1,88 +1,138 @@
 import fs from "node:fs";
 
-// todo add function for checking next character
-// todo put all scans into a variable and output it
-// todo create a type/class for tokens
+const tokenType = {
+  LEFT_PAREN: 'LEFT_PAREN',
+  RIGHT_PAREN: 'RIGHT_PAREN',
+  LEFT_BRACE: 'LEFT_BRACE',
+  RIGHT_BRACE: 'RIGHT_BRACE',
+  COMMA: 'COMMA',
+  DOT: 'DOT',
+  MINUS: 'MINUS',
+  PLUS: 'PLUS',
+  SEMICOLON: 'SEMICOLON',
+  SLASH: 'SLASH',
+  STAR: 'STAR',
+  EOF: 'EOF',
+} as const;
 
-function tokenize(filename: string) {
-  let fileContent;
+type TokenType = keyof typeof tokenType;
 
-  try {
-    fileContent = fs.readFileSync(filename, "utf8");
+type Token = {
+  type: TokenType;
+  lexeme: string;
+  literal: any;
+};
+
+class Tokenizer {
+  #tokens: Token[] = [];
+  #errors: string[] = [];
+
+  constructor(public filename: string) { }
+
+  push(type: TokenType, lexeme: string, literal: any = null): void {
+    this.#tokens.push({
+      type,
+      lexeme,
+      literal
+    });
   }
-  catch (err) {
-    if (err instanceof Error) {
-      console.error(err.message);
-    }
-    else {
-      console.error(err);
-    }
-    process.exit(1);
+
+  get tokens(): Token[] {
+    return [...this.#tokens];
   }
 
-  let hasCompileErrors = false;
+  get errors(): string[] {
+    return [...this.#errors];
+  }
 
-  const fileLines = fileContent.split('\n');
-  for (let fileLine of fileLines) {
-    for (let j = 0; j < fileLine.length; ++j)
-      switch (fileLine[j]) {
-        case '(': {
-          console.log('LEFT_PAREN ( null');
-          break;
-        }
-        case ')': {
-          console.log('RIGHT_PAREN ) null');
-          break;
-        }
-        case '{': {
-          console.log('LEFT_BRACE { null');
-          break;
-        }
-        case '}': {
-          console.log('RIGHT_BRACE } null');
-          break;
-        }
-        case ',': {
-          console.log('COMMA , null');
-          break;
-        }
-        case '.': {
-          console.log('DOT . null');
-          break;
-        }
-        case '-': {
-          console.log('MINUS - null');
-          break;
-        }
-        case '+': {
-          console.log('PLUS + null');
-          break;
-        }
-        case ';': {
-          console.log('SEMICOLON ; null');
-          break;
-        }
-        case '/': {
-          console.log('SLASH / null');
-          break;
-        }
-        case '*': {
-          console.log('STAR * null');
-          break;
-        }
-        default: {
-          hasCompileErrors = true;
-          console.error(`[line 1] Error: Unexpected character: ${fileLine[j]}`);
+  output(): void {
+    this.#tokens.forEach(token => {
+      console.log(token.type, token.lexeme, token.literal);
+    });
+    this.#errors.forEach((error) => {
+      console.error(error);
+    });
+  }
+
+  getFileContents(): string {
+    try {
+      return fs.readFileSync(this.filename, "utf8");
+    }
+    catch (err) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      }
+      else {
+        console.error(err);
+      }
+      process.exit(1);
+    }
+  }
+
+  tokenize(): void {
+    const fileContents = this.getFileContents();
+    const fileLines = fileContents.split('\n');
+
+    for (let [i, fileLine] of fileLines.entries()) {
+      for (let j = 0; j < fileLine.length; ++j) {
+
+        switch (fileLine[j]) {
+          case '(': {
+            this.push('LEFT_PAREN', '(');
+            break;
+          }
+          case ')': {
+            this.push('RIGHT_PAREN', ')');
+            break;
+          }
+          case '{': {
+            this.push('LEFT_BRACE', '{');
+            break;
+          }
+          case '}': {
+            this.push('RIGHT_BRACE', '}');
+            break;
+          }
+          case ',': {
+            this.push('COMMA', ',');
+            break;
+          }
+          case '.': {
+            this.push('DOT', '.');
+            break;
+          }
+          case '-': {
+            this.push('MINUS', '-');
+            break;
+          }
+          case '+': {
+            this.push('PLUS', '+');
+            break;
+          }
+          case ';': {
+            this.push('SEMICOLON', ';');
+            break;
+          }
+          case '/': {
+            this.push('SLASH', '/');
+            break;
+          }
+          case '*': {
+            this.push('STAR', '*');
+            break;
+          }
+          default: {
+            this.#errors.push(`[line ${i + 1}] Error: Unexpected character: ${fileLine[j]}`);
+          }
         }
       }
-  }
+    }
 
-  console.log("EOF  null");
-
-  if (hasCompileErrors) {
-    process.exit(65);
+    this.push('EOF', '', null);
   }
 }
+
+// todo add function for checking next character
 
 function main() {
   const args = process.argv.slice(2);
@@ -93,20 +143,23 @@ function main() {
   }
 
   const command = args[0];
-
   if (command !== "tokenize") {
     console.error(`Usage: Unknown command: ${command}`);
     process.exit(1);
   }
 
   const filename = args[1];
-
   if (!filename) {
     console.error(`No file specified for tokenizer`);
     process.exit(1);
   }
 
-  tokenize(filename);
+  const tokenizer = new Tokenizer(filename);
+  tokenizer.tokenize();
+  tokenizer.output();
+  if (tokenizer.errors.length > 0) {
+    process.exit(65);
+  }
 }
 
 main();
