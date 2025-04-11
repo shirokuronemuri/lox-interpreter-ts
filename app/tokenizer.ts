c7d2e06e665bd83d3e0cfecd75ba1366b42bb210
Ihor Pavliuk <akira.pavl@gmail.com>
2025-04-11 23:51:48 +0300

    fixed multiline string tokenizing

--- app/tokenizer.ts
+++ app/tokenizer.ts
@@ -56,182 +56,161 @@ export class Tokenizer {
 
   tokenize(): void {
     const fileContents = this.#getFileContents();
-    const fileLines = fileContents.split('\n');
+    let line = 1;
+    for (let i = 0; i < fileContents.length; ++i) {
+      let ignoreLine = false;
 
-    for (let i = 0; i < fileLines.length; ++i) {
-      const fileLine = fileLines[i]!;
-      for (let j = 0; j < fileLine.length; ++j) {
-        let ignoreLine = false;
-
-        switch (fileLine[j]) {
-          case '"': {
-            // let resultString = "";
-            // ++j;
-            // while (true) {
-            //   if (i >= fileLines.length) {
-            //     ErrorReporter.report(i - 1, "", 'Unterminated string.');
-            //     break;
-            //   }
-            //   else if (j >= fileLine.length) {
-            //     resultString += '\n';
-            //     ++i;
-            //     j = 0;
-            //   }
-            //   else if (fileLines[i]![j] === '"') {
-            //     this.push('STRING', `"${resultString}"`, i + 1, resultString);
-            //     break;
-            //   }
-            //   else {
-            //     resultString += fileLines[i]![j];
-            //     ++j;
-            //   }
-            // }
-            // TODO ADD TOKENIZING MULTILINE STRINGS, PROBABLY REWRITE THE WHOLE LOOP THING
-            const stringEndIndex = fileLine.indexOf('"', j + 1);
-            if (stringEndIndex === -1) {
-              ErrorReporter.report(i + 1, "", 'Unterminated string.');
-            }
-            else {
-              const stringLiteral = fileLine.slice(j + 1, stringEndIndex);
-              j = stringEndIndex;
-              this.push('STRING', `"${stringLiteral}"`, i + 1, stringLiteral);
-            }
-            break;
-          }
-          case ' ': {
-            continue;
-          }
-          case '\t': {
-            continue;
-          }
-          case '/': {
-            if (this.#checkNextCharacter(fileLine, j, '/')) {
-              ignoreLine = true;
-            }
-            else {
-              this.push('SLASH', '/', i + 1);
-            }
-            break;
-          }
-          case '>': {
-            if (this.#checkNextCharacter(fileLine, j, '=')) {
-              this.push('GREATER_EQUAL', '>=', i + 1);
-              ++j;
-            }
-            else {
-              this.push('GREATER', '>', i + 1);
-            }
-            break;
-          }
-          case '<': {
-            if (this.#checkNextCharacter(fileLine, j, '=')) {
-              this.push('LESS_EQUAL', '<=', i + 1);
-              ++j;
-            }
-            else {
-              this.push('LESS', '<', i + 1);
-            }
-            break;
-          }
-          case '!': {
-            if (this.#checkNextCharacter(fileLine, j, '=')) {
-              this.push('BANG_EQUAL', '!=', i + 1);
-              ++j;
-            }
-            else {
-              this.push('BANG', '!', i + 1);
-            }
-            break;
-          }
-          case '=': {
-            if (this.#checkNextCharacter(fileLine, j, '=')) {
-              this.push('EQUAL_EQUAL', '==', i + 1);
-              ++j;
-            }
-            else {
-              this.push('EQUAL', '=', i + 1);
-            }
-            break;
-          }
-          case '(': {
-            this.push('LEFT_PAREN', '(', i + 1);
-            break;
-          }
-          case ')': {
-            this.push('RIGHT_PAREN', ')', i + 1);
-            break;
-          }
-          case '{': {
-            this.push('LEFT_BRACE', '{', i + 1);
-            break;
-          }
-          case '}': {
-            this.push('RIGHT_BRACE', '}', i + 1);
-            break;
-          }
-          case ',': {
-            this.push('COMMA', ',', i + 1);
-            break;
-          }
-          case '.': {
-            this.push('DOT', '.', i + 1);
-            break;
-          }
-          case '-': {
-            this.push('MINUS', '-', i + 1);
-            break;
-          }
-          case '+': {
-            this.push('PLUS', '+', i + 1);
-            break;
-          }
-          case ';': {
-            this.push('SEMICOLON', ';', i + 1);
-            break;
-          }
-          case '/': {
-            this.push('SLASH', '/', i + 1);
-            break;
-          }
-          case '*': {
-            this.push('STAR', '*', i + 1);
-            break;
-          }
-          default: {
-            const numberLiteralRegex = /^(\d+)(?:\.(\d+))?/;
-            const numberMatch = fileLine.slice(j).match(numberLiteralRegex);
-
-            const reservedWordRegex = new RegExp(`^(${Object.values(reservedWords).join('|')})`);
-            const reservedWordMatch = fileLine.slice(j).match(reservedWordRegex);
-
-            const identifierRegex = /^[a-zA-Z_][a-zA-Z_0-9]*/;
-            const identifierMatch = fileLine.slice(j).match(identifierRegex);
-            if (numberMatch && numberMatch[0]) {
-              this.push('NUMBER', numberMatch[0], i + 1, parseFloat(numberMatch[0]));
-              j += numberMatch[0].length - 1;
-            }
-            else if (reservedWordMatch && reservedWordMatch[0]) {
-              const token = Object.keys(reservedWords)
-                .find((key) => reservedWords[key as ReservedWord] === reservedWordMatch[0]) as ReservedWord;
-              this.push(token, reservedWordMatch[0], i + 1);
-              j += reservedWordMatch[0].length - 1;
-            }
-            else if (identifierMatch && identifierMatch[0]) {
-              this.push('IDENTIFIER', identifierMatch[0], i + 1);
-              j += identifierMatch[0].length - 1;
-            }
-            else {
-              ErrorReporter.report(i + 1, "", `Unexpected character: ${fileLine[j]}`);
-            }
-          }
+      switch (fileContents[i]) {
+        case '\n': {
+          ++line;
+          continue;
         }
-
-        if (ignoreLine) {
+        case '"': {
+          const stringEndIndex = fileContents.indexOf('"', i + 1);
+          if (stringEndIndex === -1) {
+            ErrorReporter.report(line, "", 'Unterminated string.');
+          }
+          else {
+            const stringLiteral = fileContents.slice(i + 1, stringEndIndex);
+            i = stringEndIndex;
+            line += stringLiteral.split('\n').length - 1;
+            this.push('STRING', `"${stringLiteral}"`, line, stringLiteral);
+          }
           break;
         }
+        case ' ': {
+          continue;
+        }
+        case '\t': {
+          continue;
+        }
+        case '/': {
+          if (this.#checkNextCharacter(fileContents, i, '/')) {
+            ignoreLine = true;
+          }
+          else {
+            this.push('SLASH', '/', line);
+          }
+          break;
+        }
+        case '>': {
+          if (this.#checkNextCharacter(fileContents, i, '=')) {
+            this.push('GREATER_EQUAL', '>=', line);
+            ++i;
+          }
+          else {
+            this.push('GREATER', '>', line);
+          }
+          break;
+        }
+        case '<': {
+          if (this.#checkNextCharacter(fileContents, i, '=')) {
+            this.push('LESS_EQUAL', '<=', line);
+            ++i;
+          }
+          else {
+            this.push('LESS', '<', line);
+          }
+          break;
+        }
+        case '!': {
+          if (this.#checkNextCharacter(fileContents, i, '=')) {
+            this.push('BANG_EQUAL', '!=', line);
+            ++i;
+          }
+          else {
+            this.push('BANG', '!', line);
+          }
+          break;
+        }
+        case '=': {
+          if (this.#checkNextCharacter(fileContents, i, '=')) {
+            this.push('EQUAL_EQUAL', '==', line);
+            ++i;
+          }
+          else {
+            this.push('EQUAL', '=', line);
+          }
+          break;
+        }
+        case '(': {
+          this.push('LEFT_PAREN', '(', line);
+          break;
+        }
+        case ')': {
+          this.push('RIGHT_PAREN', ')', line);
+          break;
+        }
+        case '{': {
+          this.push('LEFT_BRACE', '{', line);
+          break;
+        }
+        case '}': {
+          this.push('RIGHT_BRACE', '}', line);
+          break;
+        }
+        case ',': {
+          this.push('COMMA', ',', line);
+          break;
+        }
+        case '.': {
+          this.push('DOT', '.', line);
+          break;
+        }
+        case '-': {
+          this.push('MINUS', '-', line);
+          break;
+        }
+        case '+': {
+          this.push('PLUS', '+', line);
+          break;
+        }
+        case ';': {
+          this.push('SEMICOLON', ';', line);
+          break;
+        }
+        case '/': {
+          this.push('SLASH', '/', line);
+          break;
+        }
+        case '*': {
+          this.push('STAR', '*', line);
+          break;
+        }
+        default: {
+          const numberLiteralRegex = /^(\d+)(?:\.(\d+))?/;
+          const numberMatch = fileContents.slice(i).match(numberLiteralRegex);
+
+          const reservedWordRegex = new RegExp(`^(${Object.values(reservedWords).join('|')})`);
+          const reservedWordMatch = fileContents.slice(i).match(reservedWordRegex);
+
+          const identifierRegex = /^[a-zA-Z_][a-zA-Z_0-9]*/;
+          const identifierMatch = fileContents.slice(i).match(identifierRegex);
+          if (numberMatch && numberMatch[0]) {
+            this.push('NUMBER', numberMatch[0], line, parseFloat(numberMatch[0]));
+            i += numberMatch[0].length - 1;
+          }
+          else if (reservedWordMatch && reservedWordMatch[0]) {
+            const token = Object.keys(reservedWords)
+              .find((key) => reservedWords[key as ReservedWord] === reservedWordMatch[0]) as ReservedWord;
+            this.push(token, reservedWordMatch[0], line);
+            i += reservedWordMatch[0].length - 1;
+          }
+          else if (identifierMatch && identifierMatch[0]) {
+            this.push('IDENTIFIER', identifierMatch[0], line);
+            i += identifierMatch[0].length - 1;
+          }
+          else {
+            ErrorReporter.report(line, "", `Unexpected character: ${fileContents[i]}`);
+          }
+        }
+      }
+
+      if (ignoreLine) {
+        break;
       }
     }
 
-    this.push('EOF', '', fileLines.length, null);
+    this.push('EOF', '', line, null);
   }
 };
